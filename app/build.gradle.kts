25d328e308977835d9f25da2a67e28ab233c0d75
Dipesh Pal <codewithdipesh2022@gmail.com>
2025-10-01 13:30:07 +0530

    firebase auth setup and connects

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("kotlin-kapt")
+    id("com.google.gms.google-services")
     id("com.google.dagger.hilt.android")
 }
 
